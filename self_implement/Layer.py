22269f254a1e9df159d094d2d31a052508631df8
ghdry2563 <ghdry2563@gmail.com>
2021-02-26 21:30:50 +0900

    순전파 역전파 구현

---
+++ self_implement/Layer.py
@@ -0,0 +1,63 @@
+
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx, dy
+
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backword(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+print("apple : ", apple)
+print("apple_num : ", apple_num)
+print("tax : ", tax)
+
+# 계층들
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+print("순전파 : ", price) # 220
+
+
+# 역전파
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print("역전파 : ", dapple, dapple_num, dtax) # 2.2 110 200
+
+
+
+    
